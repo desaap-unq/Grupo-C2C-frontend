637c4c3ef77699309f16dcb08b7c89261fa1b6e6
gguzman89 <ggtpi0gabriel@gmail.com>
2019-10-05 16:12:52 -0300

    delete var map unused

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 
+// var map;
 export default class Map extends Component {
   componentDidMount() {
     this.initMap();
@@ -9,12 +10,11 @@ export default class Map extends Component {
     let options = {
       zoom: this.props.zoom,
       center: this.props.center,
-      //mapTypeId: google.maps.MapTypeId.ROADMAP
+      mapTypeId: window.google.maps.MapTypeId.ROADMAP
     };
-    let map = new window.google.maps.Map(
-      document.getElementById("map"),
-      options
-    );
+    //map = 
+    new window.google.maps.Map(
+      document.getElementById('map'), options);
   }
 
   render() {
