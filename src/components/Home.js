c4a57d5d35f9128ff00eb127cb19f1fe867a1ad0
gguzman89 <ggtpi0gabriel@gmail.com>
2019-10-31 19:00:45 -0300

    fix position relative map uncheck

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
     return(
         <ReactDependentScript
           scripts={[
-            'https://maps.googleapis.com/maps/api/js?key='+key+'&libraries=places'
+            'https://maps.googleapis.com/maps/api/js?key='+key+'&libraries=places,geometry'
           ]}
         >
           <Map center={{ lat: -34.706501, lng: -58.2807187 }} zoom={15} />
