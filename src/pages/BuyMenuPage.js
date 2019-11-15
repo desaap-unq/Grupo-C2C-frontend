65cda73779836791e35bb997a2377411b2685680
sergio <sergioluke35@gmail.com>
2019-11-15 12:45:31 -0300

    add data mock

--- src/pages/BuyMenuPage.js
+++ src/pages/BuyMenuPage.js
@@ -5,6 +5,23 @@ import ViandasyaHeader from "../components/Headers/Viandasya-header.js";
 import {Container} from "reactstrap";
 
 export default class BuyMenuPage extends Component{
+    business = {
+        name = "Calchaqui",
+        logo = logo,
+        locality = "Quilmes",
+        address = "Calchaqui 2345",
+        location = "Quilmes",
+        description = "parrilla familiar",
+        link = "www.laEstancia.com.ar",
+        email = "estancia_20119@gmail.com",
+        phone = "42557845",
+        schedule = "",
+        days = [1,2,3,4,5,6],
+        delivery = false,
+        walletId = "46"
+    }; 
+    menus = []; 
+
     render(){
         return(
             <div>
