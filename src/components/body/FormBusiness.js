e5926b9b4eb62eb1d38eb74503cbff514c9b5f91
gguzman89 <ggtpi0gabriel@gmail.com>
2019-11-27 21:05:26 -0300

    fix business, add FormBusinessPage

---
+++ src/components/body/FormBusiness.js
@@ -0,0 +1,106 @@
+import React, { Component } from "react";
+import { 
+  Button,
+  Row,
+  Col, 
+  CustomInput,
+  Form, FormGroup, Label, Input, FormText, FormFeedback } from 'reactstrap';
+
+class FormBusiness extends Component {
+
+  render() {
+    return (
+      <>
+        <Form>
+          <FormGroup>
+            <Label for="name">Business Name</Label>
+            <Input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="name"/>
+            <FormFeedback tooltip>You will not be able to see this</FormFeedback>
+            <FormText>Example help text that remains unchanged.</FormText>
+          </FormGroup>
+          <Row form>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="locality">Locality</Label>
+                <Input type="text" name="locality" id="locality"/>
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="address">Address</Label>
+                <Input type="text" name="address" id="address"/>
+              </FormGroup>
+            </Col>
+          </Row>
+          <FormGroup>
+            <Label for="description">Description</Label>
+            <Input type="textarea" name="description" id="description" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="link">Web Site</Label>
+            <Input
+              type="url"
+              name="link"
+              id="link"
+              placeholder="web site"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="email">Email</Label>
+            <Input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="pepe@know.com"
+            />
+          </FormGroup>
+          <Row form>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="zip">Zip</Label>
+                <Input type="number" name="zip" id="zip"/>
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="phone">Number</Label>
+                <Input
+                  type="number"
+                  name="phone"
+                  id="phone"
+                  placeholder="12345678"
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+           <FormGroup>
+            <Label for="days">Days</Label>
+            <div>
+              <CustomInput type="radio" id="days" name="days" label="LunAVier" />
+              <CustomInput type="radio" id="days" name="days" label="LunADom" />
+            </div>  
+          </FormGroup>
+          <h3>
+            funcion Sliders()
+          </h3>
+          <FormGroup row>
+            <Label for="exampleFile" sm={2}>File</Label>
+            <Col sm={10}>
+              <Input type="file" name="file" id="exampleFile" />
+              <FormText color="muted">
+                This is some placeholder block-level help text for the above input.
+                It's a bit lighter and easily wraps to a new line.
+              </FormText>
+            </Col>
+          </FormGroup>
+        </Form>
+      </>
+    );
+  }
+}
+
+export default FormBusiness;
