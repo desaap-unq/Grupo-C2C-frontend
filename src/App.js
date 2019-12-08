ac84d2677cdab3c767fb8b22aa873ef6887d67c6
gguzman89 <ggtpi0gabriel@gmail.com>
2019-12-08 19:14:12 -0300

    refactor auth0-spa to auth0-context

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React from "react";
-import NavBar from "./components/Navbars/NavBar";
-import { useAuth0 } from "./react-auth0-spa";
+// import NavBar from "./components/Navbars/NavBar";
+// import { useAuth0 } from "./react-auth0-spa";
+import "./App.css";
+import { useAuth0 } from "./contexts/auth0-context";
+import Auth0Header from "./components/Headers/Auth0Header";
 
 function App() {
-  const { loading } = useAuth0();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const {isLoading, user, loginWithRedirect, logout} = useAuth0();
 
   return (
+    <>
+    <Auth0Header />
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      {!isLoading && !user && (
+        <>
+          <h1> Clic Below! </h1>
+          <button onClick={loginWithRedirect} >
+            Login
+          </button>
+        </>
+      )}
+      {!isLoading && user && (
+        <>
+          <h1> You are Logged in! </h1>
+          <p> hello {user.name} </p>
+
+          {user.picture && <img src={user.picture} alt="My Avatar" />}
+          <hr />
+
+          <button
+            onClick={() => logout({returnTo: window.location.origin})}
+          >
+            LogOut
+          </button>  
+        </>
+      )}
     </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { Auth0Provider } from "./react-auth0-spa";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import './index.css';
+
+import * as serviceWorker from './serviceWorker';
+
+import './assets/css/bootstrap.min.css';
+import './assets/css/paper-kit.css';
+import './assets/css/viandas-ya.css';
+import './assets/demo/demo.css';
+
+import Index from './views/Index';
+import NucleoIcons from './views/NucleoIcons';
+import LandingPage from './views/examples/LandingPage';
+import ProfilePage from './views/examples/ProfilePage';
+import RegisterPage from './views/examples/RegisterPage';
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
+import BuyMenuPage from './pages/BuyMenuPage';
+import LoginPage from './pages/LoginPage';
+import LoadBusinessPage from './pages/LoadBusinessPage';
+
 import config from "./auth_config.json";
 import history from "./utils/history";
+import { Auth0Provider } from "./contexts/auth0-context";
+import App from "./App";
 
-// A function that routes the user to the right place
-// after login
+// BROWSER=chrome npm start | choose browser
+// mvn spring-boot:run
+
 const onRedirectCallback = appState => {
   history.push(
     appState && appState.targetUrl
@@ -17,12 +38,41 @@ const onRedirectCallback = appState => {
 };
 
 ReactDOM.render(
-  <Auth0Provider
-    domain={config.domain}
-    client_id={config.clientId}
-    redirect_uri={window.location.origin}
-    onRedirectCallback={onRedirectCallback}
-  >
+  // <Auth0Provider
+  //   domain={config.domain}
+  //   client_id={config.clientId}
+  //   redirect_uri={window.location.origin}
+  //   onRedirectCallback={onRedirectCallback}
+  // >
+  // <BrowserRouter>
+  //   <Switch>
+  //     <Route path="/index" render={props => <HomePage {...props} />} />,
+  //     <Route path="/search/:food" render={props => <SearchPage {...props} />} />,
+  //     <Route path="/business/:id/menu" render={props => <BuyMenuPage {...props} />} />,
+  //     <Route path="/indexExample" render={props => <Index {...props} />} />,
+  //     <Route path="/login" render={props => <LoginPage {...props} />} />,
+  //     <Route path="/business/load" render={props => <LoadBusinessPage {...props} />} />,
+  //     <Route
+  //       path="/nucleo-icons"
+  //       render={props => <NucleoIcons {...props} />}
+  //     />
+  //     <Route
+  //       path="/landing-page"
+  //       render={props => <LandingPage {...props} />}
+  //     />
+  //     <Route
+  //       path="/profile-page"
+  //       render={props => <ProfilePage {...props} />}
+  //     />
+  //     <Route
+  //       path="/register-page"
+  //       render={props => <RegisterPage {...props} />}
+  //     />
+  //     <Redirect to="/index" />
+  //   </Switch>
+  // </BrowserRouter>
+  // </Auth0Provider>,
+  <Auth0Provider>
     <App />
   </Auth0Provider>,
   document.getElementById("root")
