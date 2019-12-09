75d6612fedb1c230a6027686ad03ed4e882e9b11
gguzman89 <ggtpi0gabriel@gmail.com>
2019-12-09 18:33:44 -0300

    clean test App

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
 import React from "react";
-// import NavBar from "./components/Navbars/NavBar";
-// import { useAuth0 } from "./react-auth0-spa";
 import "./App.css";
 import { useAuth0 } from "./contexts/auth0-context";
 import Auth0Header from "./components/Headers/Auth0Header";
 
 function App() {
-  const {isLoading, user, loginWithRedirect, logout} = useAuth0();
+  // const {isLoading, user, loginWithRedirect, logout} = useAuth0();
 
   return (
-    <>
-    <Auth0Header />
-    <div className="App">
-      {!isLoading && !user && (
-        <>
+    // <>
+    // <Auth0Header />
+    // <div className="App">
+    //   {!isLoading && !user && (
+    //     <>
           <h1> Clic Below! </h1>
-          <button onClick={loginWithRedirect} >
-            Login
-          </button>
-        </>
-      )}
-      {!isLoading && user && (
-        <>
-          <h1> You are Logged in! </h1>
-          <p> hello {user.name} </p>
+    //       <button onClick={loginWithRedirect} >
+    //         Login
+    //       </button>
+    //     </>
+    //   )}
+    //   {!isLoading && user && (
+    //     <>
+    //       <h1> You are Logged in! </h1>
+    //       <p> hello {user.name} </p>
 
-          {user.picture && <img src={user.picture} alt="My Avatar" />}
-          <hr />
+    //       {user.picture && <img src={user.picture} alt="My Avatar" />}
+    //       <hr />
 
-          <button
-            onClick={() => logout({returnTo: window.location.origin})}
-          >
-            LogOut
-          </button>  
-        </>
-      )}
-    </div>
-    </>
+    //       <button
+    //         onClick={() => logout({returnTo: window.location.origin})}
+    //       >
+    //         LogOut
+    //       </button>  
+    //     </>
+    //   )}
+    // </div>
+    // </>
   );
 }
 
